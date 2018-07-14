dfe444f85edb08fc40daaefd2d1f11d955a69511
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-14 14:30:44 +0530

    Create Alphabets.java

---
+++ Alphabets.java
@@ -0,0 +1,8 @@
+ Scanner s=new Scanner(System.in);
+	  char c=s.next().charAt(0); 
+	  if((c>='a' && c<='z') ||(c>='A' && c<='Z')){
+		  System.out.println("Alphabets");
+	  }
+	  else{
+		  System.out.println("NO");
+	  }
