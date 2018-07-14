70cdd03d8a53dedd000c4004d7a2e00c83e78155
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-14 16:10:49 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,8 @@
+ Scanner s=new Scanner(System.in);
+		  int number;
+		  int power;
+		  number=s.nextInt();
+		  power=s.nextInt();
+		 int a=0;
+		 a=(int)Math.pow(number,power);
+		 System.out.println(a);
