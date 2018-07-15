510b7846d26706a77ba82a6878b9e0de2afdf1ea
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-15 11:34:04 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,12 @@
+ Scanner s=new Scanner(System.in);
+		  int number,k;
+		  number=s.nextInt();
+		  k=s.nextInt();
+		  int i,count=0;
+		  if (k<=10000){
+		  for(i=number+1;i<k;i++){
+			if(i%2==0){  
+				System.out.print(i+" ");
+			}
+	}
+		  }
