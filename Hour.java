20f115989f79042365832fce38ee2b3c444db791
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-16 19:24:28 +0530

    Create Hour.java

---
+++ Hour.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Hour{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+	    int hour1=sc.nextInt();
+	    int min1=sc.nextInt();
+        int hour2=sc.nextInt();
+        int min2=sc.nextInt();
+        System.out.println(Math.abs(hour1-hour2)+" "+Math.abs(min1-min2));
+ 
+       }
+}
