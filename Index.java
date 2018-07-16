0654ddd72257db37e1e4cc54021ee6b05905a665
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-16 14:35:03 +0530

    Create Index.java

---
+++ Index.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Index{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++){
+			System.out.println(a[i]+"  "+i);
+		}
+	}
+}
