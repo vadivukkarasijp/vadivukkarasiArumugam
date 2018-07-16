a2d2e2854dba1334b611beb4a7e51088c26fdc92
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-16 10:15:00 +0530

    Create maximumthree.java

---
+++ maximumthree.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class guviThree {
+public static void main(String[]args){
+	  Scanner s=new Scanner(System.in);
+	  int a,max=0;
+	  a=s.nextInt();
+	  int b[]=new int[a];
+	  for(int i=0;i<a;i++){
+		  b[i]=s.nextInt();
+	  }
+	  for(int i=0;i<a;i++){
+		  for(int j=1;j<a;j++){
+		  if(b[i]>b[j])
+			  max=b[i];
+			  b[i]=b[j];
+			  b[j]=max;
+	  }
+	  }
+	  System.out.println(max);
+	s.close();
+}
+}
