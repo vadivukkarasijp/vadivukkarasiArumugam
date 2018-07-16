c6eafc9bb43db8d93426ce51e40bf1dd21480f24
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-16 19:37:44 +0530

    Create SpaceCount.java

---
+++ SpaceCount.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class SpaceCount{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+	    String a=((sc.nextLine()).replaceAll(" ",""));
+	    int b=a.length();
+	    System.out.println(b);
+ 
+       }
+}
