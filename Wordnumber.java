46938e4002f553b040407766b800cecf643c1bf8
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-17 15:34:42 +0530

    Create Wordnumber.java

---
+++ Wordnumber.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Wordnumber {
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	String b=sc.next();
+	int a=sc.nextInt();
+	for(int i=0;i<a;i++){
+		System.out.println(b);
+	}
+}
+}
