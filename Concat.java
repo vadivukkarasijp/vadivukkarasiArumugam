8aff5953c2835f52ab0ac93fc7dc2ab0d1b38f60
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-18 11:17:24 +0530

    Create Concat.java

---
+++ Concat.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Concat {
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	String a=sc.next();
+	String b=sc.next();
+	System.out.println(a+b);
+}
+}
