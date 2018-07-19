29d221ae3ce17d27922f834a714c91a8a71dbc50
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-19 14:53:27 +0530

    Create printletter .java

---
+++ printletter .java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class GuviFive {
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	switch(a){
+	case 1:{
+		System.out.println("one");
+		break;
+	}
+	case 2:{
+		System.out.println("two");
+		break;
+	}
+	case 3:{
+		System.out.println("three");
+		break;
+	}
+	case 4:{
+		System.out.println("four");
+		break;
+	}
+	case 5:{
+		System.out.println("five");
+		break;
+	}
+	case 6:{
+		System.out.println("Six");
+		break;
+	}
+	case 7:{
+		System.out.println("seven");
+		break;
+	}
+	case 8:{
+		System.out.println("Eight");
+		break;
+	}
+	case 9:{
+		System.out.println("nine");
+		break;
+	}
+	case 10:{
+		System.out.println("Ten");
+		break;
+	}
+}
+}
+}
