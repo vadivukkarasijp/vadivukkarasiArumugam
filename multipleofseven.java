cd9e63d8d08b9593663b9b5b00676a6305030c95
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-20 15:35:11 +0530

    Create multipleofseven.java

---
+++ multipleofseven.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class mulitipleofseven {
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	if(a%7==0){
+		System.out.println("yes");
+	}
+	else{
+		System.out.println("no");
+	}
+}
+}
