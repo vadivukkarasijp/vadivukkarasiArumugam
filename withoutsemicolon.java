a1749604be1364d6276764f75c4e39fdec3ca585
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-20 14:53:53 +0530

    Create withoutsemicolon.java

---
+++ withoutsemicolon.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class withoutsemicolon {
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	String a=sc.next();
+	String b=a;
+	if(System.out.printf(b)==null){
+	}
+}
+}
+	
