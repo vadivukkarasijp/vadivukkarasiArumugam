f92cd4c80914ad46264d21bbfbabb586ac6c96ca
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-21 14:26:03 +0530

    Update stringvowels.java

--- stringvowels.java
+++ stringvowels.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class stringvowels.java{
+public class stringvowels{
 public static void main(String[] args){
 	Scanner sc=new Scanner(System.in);
 	 String a=sc.nextLine();
