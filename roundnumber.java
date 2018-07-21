7d23f165ac07d1f3edbae923ffc0b7e9492552ea
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-21 17:56:21 +0530

    Create roundnumber.java

---
+++ roundnumber.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class roundnumber {
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	float a=sc.nextFloat();
+	System.out.println((int)a+1);
+	}
+}
