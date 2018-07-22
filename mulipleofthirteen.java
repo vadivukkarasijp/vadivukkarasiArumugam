01d38f600e87e75f277aebf9de00ad6f957d2bc3
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-22 10:38:39 +0530

    Create mulipleofthirteen.java

---
+++ mulipleofthirteen.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class multipleofthirteen {
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int i,j;
+	int count=0;
+		if(a%13==0){
+			System.out.println("yes");
+		}
+		else{
+			System.out.println("no");
+		}
+}
+}
+
