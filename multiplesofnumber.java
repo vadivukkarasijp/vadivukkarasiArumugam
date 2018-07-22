5270943560202cc80d74f755cb3ee1d4e540035d
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-22 10:35:52 +0530

    Update and rename multiples.java to multiplesofnumber.java

---
+++ multiplesofnumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class multiplesofnumber {
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int i,j;
+	int count=0;
+	for(i=1;i<=a;i++){
+		if(a%i==0){
+			System.out.println(i);
+		}
+}}
+}
