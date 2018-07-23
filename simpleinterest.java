551f3d0e60683bd3fb0ad093b6711560379402a4
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-23 11:53:59 +0530

    Create simpleinterest.java

---
+++ simpleinterest.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class simpleinterest {
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	int p=sc.nextInt();
+	int t=sc.nextInt();
+	int r=sc.nextInt();
+	System.out.println((p*t*r)/100);
+}
+}
