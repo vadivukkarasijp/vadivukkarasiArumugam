f3e68ac75f94e6df7768faa2adb0ab3ba99c19aa
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-23 14:59:51 +0530

    Create mulmod.java

---
+++ mulmod.java
@@ -0,0 +1,11 @@
+package programs;
+import java.util.Scanner;
+public class mulmod{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		System.out.println((a*b)%c);
+	}
+}
