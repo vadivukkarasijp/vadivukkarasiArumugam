3a5e9a7cb479e304a46588bb34f5a5d35175872d
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-07-23 11:30:27 +0530

    Create kelvinrange.java

---
+++ kelvinrange.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class kelvinrange {
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	System.out.println(a+273);
+}
+}
