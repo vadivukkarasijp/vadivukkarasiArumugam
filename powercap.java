c9e1f757dbdeaffa3ed8126ee8871eca7b41b75d
vadivukkarasijp <40274288+vadivukkarasijp@users.noreply.github.com>
2018-08-09 15:28:43 +0530

    Create powercap.java

---
+++ powercap.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class powercap {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	System.out.println((int)Math.pow(a,b));}
+}
